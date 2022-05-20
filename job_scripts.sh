054399da0dd6ad71ce03bf80d4dda6564c402e92
Mike Zhu <12051594@life.hkbu.edu.hk>
2022-05-20 10:12:20 +0800

    Tidy up the code of the project

--- job_scripts.sh
+++
@@ -1,6 +0,0 @@
-module load anaconda/3
-# conda update -n base conda
-# conda env create -f environment.yml
-# conda env list
-conda activate fed3
-python3 main.py --dataset=rotate_cifar --algorithm=arith --num_rounds=101 --num_restarts=1 --learning_rate=0.0003
